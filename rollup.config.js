d06ee5de75ef1071bb29f205815344b3deeeefa7
Peter Lee <peterlee@Peters-MacBook-Air.local>
2020-01-20 16:25:25 -0800

    Add rollup postcss

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
-import resolve from '@rollup/plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import postcss from 'rollup-plugin-postcss'
 
 export default {
   input: 'src/index.js',
@@ -8,16 +9,21 @@ export default {
       name: 'react-showreel',
       file: 'dist/umd.js',
       format: 'umd'
-    }, {
+    },
+    {
       file: 'dist/cjs.js',
       format: 'cjs'
     }
   ],
   plugins: [
+    postcss({
+      extract: false,
+      modules: true
+    }),
     resolve(),
     babel({
       exclude: 'node_modules/**'
     })
   ],
-  external: ['react', 'react-dom', 'styled-components']
-};
+  external: ['react', 'react-dom']
+}
